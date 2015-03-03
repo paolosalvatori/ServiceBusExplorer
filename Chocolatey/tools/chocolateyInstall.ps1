2cccabddeb52f32c7fa5cc0442bb8a904ea32bc4
Lee Harold <lcharold@hotmail.com>
2015-03-03 15:05:16 -0600

    add Chocolatey package creation files

---
+++ Chocolatey/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$name   = "AzureServiceBusExplorer"
+$url    = "https://code.msdn.microsoft.com/windowsazure/Service-Bus-Explorer-f2abca5a/file/134519/2/Service%20Bus%20Explorer.zip"
+$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage $name $url $unzipLocation
