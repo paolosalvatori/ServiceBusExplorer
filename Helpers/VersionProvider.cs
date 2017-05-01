62d498f7ba8729ae034fabefe8dfd8f4bbad67cb
Sean Feldman <feldman.sean@gmail.com>
2017-04-30 18:06:32 -0600

    provide version with SHA1 on about

--- Helpers/VersionProvider.cs
+++ Helpers/VersionProvider.cs
@@ -7,9 +7,21 @@ namespace Microsoft.Azure.ServiceBusExplorer.Helpers
     {
         public static string GetVersion()
         {
+            var versionInfo = GetFileVersionInfo();
+
+            return $"{versionInfo.FileMajorPart}.{versionInfo.FileMinorPart}.{versionInfo.FileBuildPart}";
+        }
+
+        public static string GetVersionWithSha()
+        {
+            return GetFileVersionInfo().ProductVersion;
+        }
+
+        private static FileVersionInfo GetFileVersionInfo()
+        {
             var asm = Assembly.GetExecutingAssembly();
             var versionInfo = FileVersionInfo.GetVersionInfo(asm.Location);
-            return $"{versionInfo.FileMajorPart}.{versionInfo.FileMinorPart}.{versionInfo.FileBuildPart}";
+            return versionInfo;
         }
     }
 }
