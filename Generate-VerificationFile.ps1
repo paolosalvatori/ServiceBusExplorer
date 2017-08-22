71f32492b8f47c6cdd1a6a523109704837d172a1
Tom Kerkhove <kerkhove.tom@gmail.com>
2017-08-22 12:00:16 +0200

    Use POSH script to hash

---
+++ Generate-VerificationFile.ps1
@@ -0,0 +1,25 @@
+param(
+    [Parameter(Mandator=$true, HelpMessage="Location to output the result to")]
+    [string] $OutputFilePath
+)
+
+cd $($env:APPVEYOR_BUILD_FOLDER)\src\ServiceBusExplorer\bin\Release
+
+# Get all files
+$files = Get-ChildItem -File
+
+# Create a string builder to append all checksums
+$verificationBuilder = New-Object System.Text.StringBuilder
+
+# Loop all files to hash and append to string builder
+foreach ($file in $files) {
+    $hashResult = Get-FileHash $file -Algorithm MD5
+    $verificationBuilder.AppendLine("- $($file.Name) - $($hashResult.Hash)")
+
+    Write-Host "Processed $($file.Name)"
+}
+
+# Store output in VERIFICATION file
+New-Item -Path $OutputFilePath -Value $verificationBuilder.ToString() -Force
+
+Write-Host "Wrote results to $($OutputFilePath)"
