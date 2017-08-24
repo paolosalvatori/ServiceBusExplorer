ce5d48bb02163a538e5571e6af2505dc5e0de83e
Tom Kerkhove <kerkhove.tom@gmail.com>
2017-08-24 18:29:34 +0200

    Move to output and back to original location

--- Generate-VerificationFile.ps1
+++ Generate-VerificationFile.ps1
@@ -3,8 +3,12 @@ param(
     [string] $OutputFilePath
 )
 
+# Store original location and move to output
+$originalLocation = Get-Location
+Set-Location "$($env:APPVEYOR_BUILD_FOLDER)\src\ServiceBusExplorer\bin\Release"
+
 # Get all files
-$files = Get-ChildItem "$($env:APPVEYOR_BUILD_FOLDER)\src\ServiceBusExplorer\bin\Release" -File
+$files = Get-ChildItem -File
 
 # Create a string builder to append all checksums
 $verificationBuilder = New-Object System.Text.StringBuilder
@@ -19,5 +23,8 @@ foreach ($file in $files) {
 
 # Store output in VERIFICATION file
 New-Item -Path $OutputFilePath -Value $verificationBuilder.ToString() -Force
+
+# Move back to original location
+Set-Location $originalLocation
 
 Write-Host "Wrote results to $($OutputFilePath)"
