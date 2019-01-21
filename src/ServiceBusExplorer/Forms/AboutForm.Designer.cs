175349e4c515a703feb58866b8335e21b4dec591
Erik Mogensen <erik.mogensen@outlook.com>
2019-01-21 19:31:47 +0100

    Changed spacing of the text in the About box.

--- src/ServiceBusExplorer/Forms/AboutForm.Designer.cs
+++ src/ServiceBusExplorer/Forms/AboutForm.Designer.cs
@@ -74,10 +74,9 @@ namespace Microsoft.Azure.ServiceBusExplorer.Forms
             // 
             this.lblCorporation.AutoSize = true;
             this.lblCorporation.BackColor = System.Drawing.Color.Transparent;
-            this.lblCorporation.Location = new System.Drawing.Point(204, 327);
-            this.lblCorporation.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCorporation.Location = new System.Drawing.Point(177, 277);
             this.lblCorporation.Name = "lblCorporation";
-            this.lblCorporation.Size = new System.Drawing.Size(135, 16);
+            this.lblCorporation.Size = new System.Drawing.Size(107, 13);
             this.lblCorporation.TabIndex = 39;
             this.lblCorporation.Text = "Microsoft Corporation";
             // 
@@ -86,10 +85,9 @@ namespace Microsoft.Azure.ServiceBusExplorer.Forms
             this.lblName.AutoSize = true;
             this.lblName.BackColor = System.Drawing.Color.Transparent;
             this.lblName.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(204, 302);
-            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblName.Location = new System.Drawing.Point(177, 258);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(161, 22);
+            this.lblName.Size = new System.Drawing.Size(117, 16);
             this.lblName.TabIndex = 37;
             this.lblName.Text = "Paolo Salvatori";
             // 
@@ -97,10 +95,9 @@ namespace Microsoft.Azure.ServiceBusExplorer.Forms
             // 
             this.lblExeVersion.AutoSize = true;
             this.lblExeVersion.BackColor = System.Drawing.Color.Transparent;
-            this.lblExeVersion.Location = new System.Drawing.Point(204, 185);
-            this.lblExeVersion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblExeVersion.Location = new System.Drawing.Point(177, 146);
             this.lblExeVersion.Name = "lblExeVersion";
-            this.lblExeVersion.Size = new System.Drawing.Size(97, 16);
+            this.lblExeVersion.Size = new System.Drawing.Size(81, 13);
             this.lblExeVersion.TabIndex = 42;
             this.lblExeVersion.Text = "Version: 1.0.0.0";
             // 
@@ -109,10 +106,9 @@ namespace Microsoft.Azure.ServiceBusExplorer.Forms
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(204, 160);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(177, 126);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(220, 22);
+            this.label2.Size = new System.Drawing.Size(158, 16);
             this.label2.TabIndex = 41;
             this.label2.Text = "Service Bus Explorer";
             // 
@@ -120,10 +116,9 @@ namespace Microsoft.Azure.ServiceBusExplorer.Forms
             // 
             this.mailLinkLabel.AutoSize = true;
             this.mailLinkLabel.BackColor = System.Drawing.Color.Transparent;
-            this.mailLinkLabel.Location = new System.Drawing.Point(330, 348);
-            this.mailLinkLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.mailLinkLabel.Location = new System.Drawing.Point(272, 294);
             this.mailLinkLabel.Name = "mailLinkLabel";
-            this.mailLinkLabel.Size = new System.Drawing.Size(147, 16);
+            this.mailLinkLabel.Size = new System.Drawing.Size(114, 13);
             this.mailLinkLabel.TabIndex = 43;
             this.mailLinkLabel.TabStop = true;
             this.mailLinkLabel.Text = "paolos@microsoft.com";
@@ -133,10 +128,9 @@ namespace Microsoft.Azure.ServiceBusExplorer.Forms
             // 
             this.blogLinkLabel.AutoSize = true;
             this.blogLinkLabel.BackColor = System.Drawing.Color.Transparent;
-            this.blogLinkLabel.Location = new System.Drawing.Point(330, 368);
-            this.blogLinkLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.blogLinkLabel.Location = new System.Drawing.Point(272, 310);
             this.blogLinkLabel.Name = "blogLinkLabel";
-            this.blogLinkLabel.Size = new System.Drawing.Size(185, 16);
+            this.blogLinkLabel.Size = new System.Drawing.Size(150, 13);
             this.blogLinkLabel.TabIndex = 44;
             this.blogLinkLabel.TabStop = true;
             this.blogLinkLabel.Text = "http://blogs.msdn.com/paolos";
@@ -146,10 +140,9 @@ namespace Microsoft.Azure.ServiceBusExplorer.Forms
             // 
             this.twitterLinkLabel.AutoSize = true;
             this.twitterLinkLabel.BackColor = System.Drawing.Color.Transparent;
-            this.twitterLinkLabel.Location = new System.Drawing.Point(330, 387);
-            this.twitterLinkLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.twitterLinkLabel.Location = new System.Drawing.Point(272, 325);
             this.twitterLinkLabel.Name = "twitterLinkLabel";
-            this.twitterLinkLabel.Size = new System.Drawing.Size(175, 16);
+            this.twitterLinkLabel.Size = new System.Drawing.Size(145, 13);
             this.twitterLinkLabel.TabIndex = 45;
             this.twitterLinkLabel.TabStop = true;
             this.twitterLinkLabel.Text = "https://twitter.com/babosbird";
@@ -160,10 +153,9 @@ namespace Microsoft.Azure.ServiceBusExplorer.Forms
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(205, 347);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(178, 293);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 18);
+            this.label1.Size = new System.Drawing.Size(47, 13);
             this.label1.TabIndex = 46;
             this.label1.Text = "Email:";
             // 
@@ -172,10 +164,9 @@ namespace Microsoft.Azure.ServiceBusExplorer.Forms
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(205, 366);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(178, 308);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 18);
+            this.label4.Size = new System.Drawing.Size(39, 13);
             this.label4.TabIndex = 47;
             this.label4.Text = "Blog:";
             // 
@@ -184,10 +175,9 @@ namespace Microsoft.Azure.ServiceBusExplorer.Forms
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(205, 386);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(178, 325);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(73, 18);
+            this.label5.Size = new System.Drawing.Size(57, 13);
             this.label5.TabIndex = 48;
             this.label5.Text = "Twitter:";
             // 
@@ -195,22 +185,20 @@ namespace Microsoft.Azure.ServiceBusExplorer.Forms
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(205, 246);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(177, 214);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(108, 18);
+            this.label3.Size = new System.Drawing.Size(93, 16);
             this.label3.TabIndex = 50;
-            this.label3.Text = "Project site:";
+            this.label3.Text = "GitHub URL:";
             // 
             // siteLinkLabel
             // 
             this.siteLinkLabel.AutoSize = true;
             this.siteLinkLabel.BackColor = System.Drawing.Color.Transparent;
-            this.siteLinkLabel.Location = new System.Drawing.Point(330, 248);
-            this.siteLinkLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.siteLinkLabel.Location = new System.Drawing.Point(178, 230);
             this.siteLinkLabel.Name = "siteLinkLabel";
-            this.siteLinkLabel.Size = new System.Drawing.Size(325, 16);
+            this.siteLinkLabel.Size = new System.Drawing.Size(262, 13);
             this.siteLinkLabel.TabIndex = 49;
             this.siteLinkLabel.TabStop = true;
             this.siteLinkLabel.Text = "https://github.com/paolosalvatori/ServiceBusExplorer";
@@ -220,10 +208,9 @@ namespace Microsoft.Azure.ServiceBusExplorer.Forms
             // 
             this.lblClientVersion.AutoSize = true;
             this.lblClientVersion.BackColor = System.Drawing.Color.Transparent;
-            this.lblClientVersion.Location = new System.Drawing.Point(204, 226);
-            this.lblClientVersion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblClientVersion.Location = new System.Drawing.Point(177, 188);
             this.lblClientVersion.Name = "lblClientVersion";
-            this.lblClientVersion.Size = new System.Drawing.Size(191, 16);
+            this.lblClientVersion.Size = new System.Drawing.Size(152, 13);
             this.lblClientVersion.TabIndex = 52;
             this.lblClientVersion.Text = "Version: Could not be retrieved";
             // 
@@ -232,21 +219,20 @@ namespace Microsoft.Azure.ServiceBusExplorer.Forms
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(204, 201);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Location = new System.Drawing.Point(177, 170);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(193, 22);
+            this.label7.Size = new System.Drawing.Size(139, 16);
             this.label7.TabIndex = 51;
             this.label7.Text = "Service Bus Client";
             // 
             // AboutForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(228)))), ((int)(((byte)(242)))));
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(779, 543);
+            this.ClientSize = new System.Drawing.Size(584, 441);
             this.Controls.Add(this.lblClientVersion);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label3);
@@ -265,7 +251,6 @@ namespace Microsoft.Azure.ServiceBusExplorer.Forms
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AboutForm";
