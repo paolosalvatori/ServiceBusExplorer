d9612f19647767b9b6bc767bb00ab974b8f29cf5
Lenard Gunda <lenard@offbeat.fi>
2019-10-15 13:25:13 +0300

    Prevent using an entity-specific connection string

---
+++ src/ServiceBusExplorer/Helpers/ConnectionStringHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.ServiceBusExplorer.Helpers
+{
+    public static class ConnectionStringHelper
+    {
+        private readonly static Regex EntityPathRegex = new Regex(@"(?:^|[^\w])(EntityPath=[^;]+)");
+        
+        public static bool IsEntitySpecific(string connectionString)
+        {
+            return EntityPathRegex.IsMatch(connectionString);
+        }
+    }
+}
