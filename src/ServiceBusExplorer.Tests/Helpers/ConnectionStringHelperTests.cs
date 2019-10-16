c9794328817a581b53f5e75ad60ac5f4f11e34d3
Lenard Gunda <lenard@offbeat.fi>
2019-10-16 08:06:32 +0300

    Added units tests for ConnectionStringHelper class

---
+++ src/ServiceBusExplorer.Tests/Helpers/ConnectionStringHelperTests.cs
@@ -0,0 +1,45 @@
+﻿#region Using Directives
+
+using Microsoft.Azure.ServiceBusExplorer.Helpers;
+using NUnit.Framework;
+
+#endregion
+
+namespace Microsoft.Azure.ServiceBusExplorer.Tests.Helpers
+{
+    [TestFixture]
+    public class ConnectionStringHelperTests
+    {
+        [Test]
+        public void IsEntitySpecific_NoEntityPath_ReturnsFalse()
+        {
+            string connectionString = "Endpoint=sb://some-namespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ejfklwejfwlkejfwfflwkejfklwef";
+
+            Assert.IsFalse(ConnectionStringHelper.IsEntitySpecific(connectionString));
+        }
+
+        [Test]
+        public void IsEntitySpecific_EntityPathInTheEnd_ReturnsTrue()
+        {
+            string connectionString = "Endpoint=sb://some-namespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ejfklwejfwlkejfwfflwkejfklwef;EntityPath=loremipsum";
+
+            Assert.IsTrue(ConnectionStringHelper.IsEntitySpecific(connectionString));
+        }
+
+        [Test]
+        public void IsEntitySpecific_EntityPathInTheMiddle_ReturnsFalse()
+        {
+            string connectionString = "Endpoint=sb://some-namespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;EntityPath=loremipsum;SharedAccessKey=ejfklwejfwlkejfwfflwkejfklwef";
+
+            Assert.IsTrue(ConnectionStringHelper.IsEntitySpecific(connectionString));
+        }
+
+        [Test]
+        public void IsEntitySpecific_EntityPathInTheBeginning_ReturnsFalse()
+        {
+            string connectionString = "EntityPath=loremipsum;Endpoint=sb://some-namespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ejfklwejfwlkejfwfflwkejfklwef";
+
+            Assert.IsTrue(ConnectionStringHelper.IsEntitySpecific(connectionString));
+        }
+    }
+}
