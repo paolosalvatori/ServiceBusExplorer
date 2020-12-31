47dad55b7d7fca611dfbb4ba3f10a4a253cf268d
Arnaud TAMAILLON <arnaud.dev@gmail.com>
2020-12-31 09:21:25 +0100

    Enhance numeric text box configurability

--- src/ServiceBusExplorer/Controls/NumericTextBox.cs
+++ src/ServiceBusExplorer/Controls/NumericTextBox.cs
@@ -31,10 +31,6 @@ namespace ServiceBusExplorer.Controls
 {
     public class NumericTextBox : TextBox
     {
-        #region Private Fields
-        private bool allowSpace; 
-        #endregion
-
         #region Protected Methods
         // Restricts the entry of characters to digits (including hex), the negative sign,
         // the decimal point, and editing keystrokes (backspace).
@@ -49,24 +45,28 @@ namespace ServiceBusExplorer.Controls
 
             var keyInput = e.KeyChar.ToString(CultureInfo.InvariantCulture);
 
-            if (Char.IsDigit(e.KeyChar))
+
+            if (char.IsDigit(e.KeyChar))
             {
                 // Digits are OK
             }
-            else if (keyInput.Equals(decimalSeparator) || keyInput.Equals(groupSeparator) ||
-                     keyInput.Equals(negativeSign))
+            else if (keyInput.Equals(groupSeparator))
             {
-                // Decimal separator is OK
+                // group separator is OK
+            }
+            else if (AllowDecimal && keyInput.Equals(decimalSeparator))
+            {
+                // Decimal separator is OK if configured
+            }
+            else if (AllowNegative && keyInput.Equals(negativeSign))
+            {
+                // Negative is OK if configured
             }
             else if (e.KeyChar == '\b')
             {
                 // Backspace key is OK
             }
-            //    else if ((ModifierKeys & (Keys.Control | Keys.Alt)) != 0)
-            //    {
-            //     // Let the edit control handle control and alt key combinations
-            //    }
-            else if (allowSpace && e.KeyChar == ' ')
+            else if (AllowSpace && e.KeyChar == ' ')
             {
 
             }
@@ -89,12 +89,12 @@ namespace ServiceBusExplorer.Controls
             get { return Decimal.Parse(Text); }
         }
 
-        public bool AllowSpace
-        {
-            set { allowSpace = value; }
+        public bool AllowSpace { set; get; }
 
-            get { return allowSpace; }
-        } 
+        public bool AllowDecimal { set; get; } = true;
+
+        public bool AllowNegative { set; get; } = true;
+
         #endregion
     }
 }
