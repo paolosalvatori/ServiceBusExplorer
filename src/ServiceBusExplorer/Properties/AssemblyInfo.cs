e82bde218a82f428913025296b6918341a142917
Erik Mogensen <ErikMogensen@users.noreply.github.com>
2021-06-20 21:35:58 +0200

    Changed ServiceBusExplorer.csproj using wizard and then some manual fixing

--- src/ServiceBusExplorer/Properties/AssemblyInfo.cs
+++ src/ServiceBusExplorer/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Service Bus Explorer")]
-[assembly: AssemblyDescription("The Service Bus Explorer allows users to connect to a Service Bus namespace and easily administer and test the entities of an Azure Service Bus namespace. The tool provides advanced features like import/export functionality and the ability to test entities.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Paolo Salvatori")]
-[assembly: AssemblyProduct("Service Bus Explorer")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
