8b833473af6dbafe2f680c8f404b2811f45cc0af
Erik Mogensen <ErikMogensen@users.noreply.github.com>
2021-06-26 16:26:38 +0200

    Changed main project to use SDK style

--- src/ServiceBusExplorer/Properties/AssemblyInfo.cs
+++ src/ServiceBusExplorer/Properties/AssemblyInfo.cs
@@ -8,6 +8,19 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: InternalsVisibleTo("ServiceBusExplorer.Tests")]
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Service Bus Explorer")]
+[assembly: AssemblyDescription("The Service Bus Explorer allows users to connect to a Service Bus namespace and easily administer and test the entities of an Azure Service Bus namespace. The tool provides advanced features like import/export functionality and the ability to test entities.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Paolo Salvatori")]
+[assembly: AssemblyProduct("Service Bus Explorer")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b7dc6295-ee39-429e-9a97-68dc919f049b")]
 
