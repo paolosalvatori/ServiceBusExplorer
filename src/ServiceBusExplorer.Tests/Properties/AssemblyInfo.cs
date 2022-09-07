858f6154a69e7dad48a716b01cb24ed1036e0d45
ErikMogensen <ErikMogensen@users.noreply.github.com>
2022-09-07 20:08:09 +0200

    Set file version to 0.0.0.0 in the .NET Framework projects.

--- src/ServiceBusExplorer.Tests/Properties/AssemblyInfo.cs
+++ src/ServiceBusExplorer.Tests/Properties/AssemblyInfo.cs
@@ -29,4 +29,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
