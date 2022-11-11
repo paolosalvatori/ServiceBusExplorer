465f056f4e4e6f43da8c7e5c55cf86b3a7c8a740
Erik Mogensen <ErikMogensen@users.noreply.github.com>
2022-11-11 19:04:12 +0100

    Support preview releases (#698)
    * Changed set-output
    * Support beta releases (#26)

---
+++ GetNumberVersion.ps1
@@ -0,0 +1,20 @@
+# Verify it is a proper version
+param(
+    [Parameter(Mandatory=$false)]
+	[string]
+    $Version 
+)
+
+Set-StrictMode -Version 3
+
+# RegEx from https://semver.org/
+[string]$semanticRegEx = '^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
+
+if (-not ($Version -match $semanticRegEx)) {
+	throw "The version must follow semantic versioning, see https://semver.org/." + `
+		" For instance 4.3.22-beta."
+}
+
+$parts = $Version -split {$_ -eq '-' -or $_ -eq '+'}
+
+return $parts[0]
