bac86ff5e5ac0c42ece8e2109155344063cf8a13
asos-gurpreetsingh <gurpreet.singh@asos.com>
2024-09-22 21:11:23 +0100

    split tests to files with suitable names

---
+++ src/ServiceBusExplorer.Tests/AssemblyVersionShould.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using Xunit;
+
+namespace ServiceBusExplorer.Tests
+{
+    public class AssemblyVersionShould
+    {
+        private string expectedVersion;
+
+        public AssemblyVersionShould()
+        {
+#if DEBUG
+            expectedVersion = Environment.GetEnvironmentVariable("NUMBER_VERSION") ?? "0.0.0";
+#else
+            expectedVersion = Environment.GetEnvironmentVariable("NUMBER_VERSION") ??  throw new InvalidOperationException("Build number not set by environment");
+#endif
+            var version = Version.Parse(expectedVersion);
+            if (version.Revision == -1) expectedVersion += ".0";
+        }
+
+        [Fact]
+        public void BeSameAsProvidedByBuild()
+        { 
+            typeof(Program).Assembly.GetName().Version.ToString().Should().Be(expectedVersion);
+        }
+    }
+}
---
+++ src/ServiceBusExplorer.Tests/FileVersionShould.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using System;
+using System.Diagnostics;
+using Xunit;
+
+namespace ServiceBusExplorer.Tests
+{
+    public class FileVersionShould
+    {
+        private string expectedVersion;
+
+        public FileVersionShould()
+        {
+#if DEBUG
+            expectedVersion = Environment.GetEnvironmentVariable("FILE_VERSION") ?? "0.0.0.0";
+#else
+            expectedVersion = Environment.GetEnvironmentVariable("FILE_VERSION") ??  throw new InvalidOperationException("file version not set by environment");
+#endif
+        }
+
+
+        [Fact]
+        public void FileShouldHaveSameVersionAsProvidedByBuild()
+        {
+            var fileVersion = FileVersionInfo.GetVersionInfo(typeof(Program).Assembly.Location);
+
+            fileVersion.FileVersion.Should().Be(expectedVersion);
+        }
+    }
+}
