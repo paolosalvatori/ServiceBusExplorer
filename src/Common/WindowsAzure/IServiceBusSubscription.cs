76a118eb41e372c31ce81a6f43a418970380210b
Erik <7723579+supereddie@users.noreply.github.com>
2025-01-07 07:32:18 +0100

    Move Subscription management functions to separate class (#819)
    Co-authored-by: supereddie <>

---
+++ src/Common/WindowsAzure/IServiceBusSubscription.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ServiceBusExplorer.WindowsAzure
+{
+    internal interface IServiceBusSubscription : IServiceBusEntity
+    {
+        SubscriptionDescription CreateSubscription(TopicDescription topicDescription, SubscriptionDescription subscriptionDescription);
+
+        SubscriptionDescription CreateSubscription(TopicDescription topicDescription, SubscriptionDescription subscriptionDescription, RuleDescription ruleDescription);
+
+        void DeleteSubscription(string topicPath, string name);
+
+        Task DeleteSubscription(SubscriptionDescription subscriptionDescription);
+
+        Task DeleteSubscriptions(IEnumerable<SubscriptionDescription> subscriptionDescriptions);
+
+        SubscriptionDescription GetSubscription(string topicPath, string name);
+
+        Uri GetSubscriptionDeadLetterQueueUri(string topicPath, string name);
+
+        IEnumerable<SubscriptionDescription> GetSubscriptions(string topicPath);
+
+        IEnumerable<SubscriptionDescription> GetSubscriptions(string topicPath, string filter);
+
+        IEnumerable<SubscriptionDescription> GetSubscriptions(TopicDescription topic);
+
+        IEnumerable<SubscriptionDescription> GetSubscriptions(TopicDescription topic, string filter);
+
+        Uri GetSubscriptionUri(string topicPath, string name);
+
+        SubscriptionDescription UpdateSubscription(TopicDescription topicDescription, SubscriptionDescription subscriptionDescription);
+    }
+}
