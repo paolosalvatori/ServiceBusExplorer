4a53409e64d805154dbbe8083dbd98fa00bbcdb9
supereddie <>
2025-01-13 09:29:53 +0100

    Move Relay management functions to separate class

---
+++ src/Common/WindowsAzure/IServiceBusRelay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ServiceBusExplorer.WindowsAzure
+{
+    internal interface IServiceBusRelay : IServiceBusEntity
+    {
+        RelayDescription CreateRelay(RelayDescription description);
+
+        Task DeleteRelay(string path);
+
+        Task DeleteRelays(IEnumerable<string> relayServices);
+
+        RelayDescription GetRelay(string path);
+
+        IEnumerable<RelayDescription> GetRelays(int timeoutInSeconds);
+
+        Uri GetRelayUri(RelayDescription description);
+
+        RelayDescription UpdateRelay(RelayDescription description);
+    }
+}
