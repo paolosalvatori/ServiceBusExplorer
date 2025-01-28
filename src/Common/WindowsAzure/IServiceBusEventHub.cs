f555ed93894e0e8b5bddfc0c07eb055b0c94e1b8
Erik <7723579+supereddie@users.noreply.github.com>
2025-01-28 22:40:23 +0100

    Move EventHub management functions to separate class (#824)
    Co-authored-by: supereddie <>

---
+++ src/Common/WindowsAzure/IServiceBusEventHub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ServiceBusExplorer.WindowsAzure
+{
+    internal interface IServiceBusEventHub : IServiceBusEntity
+    {
+        EventHubDescription CreateEventHub(EventHubDescription description);
+        
+        void DeleteEventHub(EventHubDescription eventHubDescription);
+        
+        void DeleteEventHub(string path);
+        
+        void DeleteEventHubs(IEnumerable<string> eventHubs);
+        
+        EventHubDescription GetEventHub(string path);
+        
+        Task<IEnumerable<EventHubDescription>> GetEventHubs(int timeoutInSeconds);
+        
+        Uri GetEventHubUri(string eventHubPath);
+        
+        EventHubDescription UpdateEventHub(EventHubDescription description);
+    }
+}
