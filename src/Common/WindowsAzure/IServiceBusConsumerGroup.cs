4d2e6c862851bef9e26f4317de0b5a2066ffeee2
supereddie <>
2025-02-04 17:13:42 +0100

    Move ConsumerGroup management functions to separate class

---
+++ src/Common/WindowsAzure/IServiceBusConsumerGroup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ServiceBusExplorer.WindowsAzure
+{
+    internal interface IServiceBusConsumerGroup : IServiceBusEntity
+    {
+        ConsumerGroupDescription CreateConsumerGroup(ConsumerGroupDescription description);
+        
+        void DeleteConsumerGroup(ConsumerGroupDescription consumerGroupDescription);
+        
+        void DeleteConsumerGroup(string eventHubName, string name);
+        
+        void DeleteConsumerGroups(string eventHubName, IEnumerable<string> consumerGroups);
+        
+        ConsumerGroupDescription GetConsumerGroup(string eventHubPath, string name);
+        
+        IEnumerable<ConsumerGroupDescription> GetConsumerGroups(EventHubDescription description);
+        
+        IEnumerable<ConsumerGroupDescription> GetConsumerGroups(string path);
+        
+        Uri GetConsumerGroupUri(string eventHubName, string consumerGroupPath);
+        
+        ConsumerGroupDescription UpdateConsumerGroup(ConsumerGroupDescription description);
+    }
+}
