6b3c47297b9097536f981931545e4b9c82550a19
Erik Mogensen <ErikMogensen@users.noreply.github.com>
2026-07-19 19:46:40 +0200

    Undo changes to control files

--- src/ServiceBusExplorer/Forms/MainForm.Designer.cs
+++ src/ServiceBusExplorer/Forms/MainForm.Designer.cs
@@ -38,7 +38,6 @@ namespace ServiceBusExplorer.Forms
             this.connectUsingSASToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.connectUsingEntraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.logOutFromEntraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.logOutFromEntraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.savedConnectionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparatorMain = new System.Windows.Forms.ToolStripSeparator();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
